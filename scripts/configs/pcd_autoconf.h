14e46512a827bf65a58b0cefb09f83413909d4d7
me-oss <opensource@monitoreverywhere.com>
2014-11-13 15:17:38 -0800

    Initial OSS Commit of PCD

---
+++ scripts/configs/pcd_autoconf.h
@@ -0,0 +1,25 @@
+/*
+ * Automatically generated C config: don't edit
+ * Linux kernel version: 
+ * Mon Mar 10 11:18:20 2014
+ */
+#define AUTOCONF_INCLUDED
+#define CONFIG_PCD_PROCESS_SELF_EXCEPTION_DIRECTORY "/tmp"
+#define CONFIG_PCD_INSTALL_HEADERS_DIR_PREFIX "/tmp/pcd/usr/include"
+#define CONFIG_PCD_EXTRA_LDFLAGS "-lpthread"
+#define CONFIG_DEFCONFIG_LIST ".config"
+#define CONFIG_PCD_INSTALL_DIR_HOST "/tmp/pcd/usr/sbin"
+#define CONFIG_PCD_PROCESS_SELF_EXCEPTION_FILE "pcd_self_exception.txt"
+#define CONFIG_PCD_MAX_PARAM_SIZE 256
+#define CONFIG_PCD_SERVER_NAME "pcd-server"
+#define CONFIG_PCD_CROSS_COMPILER_PREFIX "arm-linux-"
+#define CONFIG_PCD_OWNER_ID 3085
+#define CONFIG_PCD_USE_FORK 1
+#define CONFIG_PCD_PRIORITY 1
+#define CONFIG_PCD_INSTALL_DIR_PREFIX "/tmp/pcd"
+#define CONFIG_PCD_EXTRA_CFLAGS ""
+#define CONFIG_UNAME_RELEASE "3.11.0-12-generic"
+#define CONFIG_PCD_PLATFORM_ARM 1
+#define CONFIG_PCD_CLIENTS_NAME_PREFIX "pcd-client-"
+#define CONFIG_PCD_MAX_LOG_SIZE 256
+#define CONFIG_PCD_TEMP_PATH "/tmp"
